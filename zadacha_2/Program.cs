30154b67fb97a56598c8b080f5135ce8a335e7d0
sergey <s.novak1990@mail.ru>
2023-09-23 03:29:18 +0300

    initial commit

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int MyMax(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    return b;
+}
+
+// int a=5, b=1;
+// Console.WriteLine(MyMax(a, b));
+
+// Console.WriteLine(MyMax(1, 10));
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+    Console.WriteLine("Числа равны");
+    return;
+}
+
+Console.WriteLine(MyMax(a, b) + " больше");
---
+++ zadacha_4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int MyMax(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    return b;
+}
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a == b && b == c && c == a)
+{
+    Console.WriteLine("Числа равны");
+    return;
+}
+
+Console.WriteLine(MyMax(MyMax(a, b), MyMax(b, c)) + " больше");
